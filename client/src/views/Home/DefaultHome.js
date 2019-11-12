6c7ffe1c8ea633cb37f404a2501b85c5f663d755
Rafael Alfonzo <ralfonzo.m@gmail.com>
2019-11-12 05:17:50 -0500

    Added different Home views depending on the user

---
+++ client/src/views/Home/DefaultHome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DefaultHome() {
+    return (
+        <div className="App">
+        	<p>Default home! </p>
+        </div>
+    );
+}
+
+export default DefaultHome;
