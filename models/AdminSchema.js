52ace6ae9a483d15784af983abf0e8659477dcfd
ProfSanjay <nsamlal@ufl.edu>
2019-11-14 14:41:13 -0500

    Updated base schemas, separate folder created for now to hold them while they're being fixed

--- models/AdminSchema.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
-
-var adminSchema = new Schema ({
-    id: {type: String, required: true},
-    username: {type: String, required: true},
-    password: {type: Number, required: true},
-    email :{type: String, required: true}
-});
--- models/GoodsSchema.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
-
-var goodsSchema = new Schema ({
-    id: {type: String, required: true},
-    name: {type: String, required: true},
-    price: {type: Number, required: true},
-    vendorID :{/* link this to a vendor */}
-});
