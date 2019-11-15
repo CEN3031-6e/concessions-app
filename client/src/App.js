8ee7fb128d7671902408a40617d296c1c489f6b8
Charles Draper <charlesdraper@ufl.edu>
2019-11-14 22:47:26 -0500

    Users are now redirected upon login

--- client/src/App.js
+++ client/src/App.js
@@ -12,91 +12,36 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
+
     this.state = {
-      isUser: false,
-      isVendor: false,
-      isAdmin: false,
+      user: null
     };
 
-    this.handleUser = this.handleUser.bind(this);
-    this.handleVendor = this.handleVendor.bind(this);
-    this.handleAdmin = this.handleAdmin.bind(this);
+    this.logIn = this.logIn.bind(this);
+    this.logOut = this.logOut.bind(this);
 
   }
 
-  //log in or log off 
-
-  handleUser() {
-    if (this.state.isUser === true) {
-      this.setState({
-        isUser : false
-      });
-    }
-    else {
-      this.setState({
-        isUser : true
-      });
-    }
+  logIn(user) {
+    console.log("Updating user's state");
+    this.setState({user: user});
   }
-
-  handleVendor() {
-    if (this.state.isVendor === true) {
-      this.setState({
-        isVendor : false
-      });
-    }
-    else {
-      this.setState({
-        isVendor : true
-      });
-    }
-  }
-
-  handleAdmin() {
-   if (this.state.isAdmin === true) {
-      this.setState({
-        isAdmin : false
-      });
-    }
-    else {
-      this.setState({
-        isAdmin : true
-      });
-    }
-  }
-
+  logOut = () => this.setState({user: null});
 
   render() {
+    console.log("Rerendering App.js");
+    let username = this.state.user ? this.state.user.name : '';
+    let adminPriv = this.state.user ? (this.state.user.email === 'cen3031@ufl.edu' && this.state.user.password === 'group6eROX') : false;
     return (
       <div>
-        <Header isUser = {this.state.isUser} isVendor = {this.state.isVendor} isAdmin = {this.state.isAdmin}/>
+        <Header loginState={this.state.user} logOut={this.logOut}/>
         <Switch>
 
-          <Route
-            exact path="/home" 
-            render={() => 
-              <Home isUser = {this.state.isUser} isVendor = {this.state.isVendor} isAdmin = {this.state.isAdmin} />
-            }
-          />
-         
-          <Route exact path="/">
-            <Redirect to="/home" />
-          </Route>
-
-           <Route 
-              exact path="/login"
-              render={() => 
-                <Login 
-                    handleUser={this.handleUser}
-                    handleVendor={this.handleVendor}
-                 />
-              }
-          />
-
-          <Route path="/register" component={Register} />
-
-          <Route path="/users" component={User} />
-
+          <Route exact path="/home" component={Home}/>
+          <Route exact path="/"><Redirect to="/home"/></Route>
+          <Route exact path="/login" render={() => <Login logIn={this.logIn}/>}/>
+          <Route exact path="/register" render={() => <Register />}/>
+          <Route path="/users" render={() => <User username={username} adminPriv={adminPriv}/>}/>
           <Route component={NotFound}/>
 
         </Switch>
