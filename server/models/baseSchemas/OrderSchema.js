e8eb9457f86676e812809d46ddd09fa08d0160d3
ProfSanjay <32143329+ProfSanjay@users.noreply.github.com>
2019-11-14 21:28:23 -0500

    Order Schema (#14)
    * Updated base schemas, separate folder created for now to hold them while they're being fixed
    
    * Base Schemas w/ Arrays (#11)
    
    * Pushed content down to allow space for header
    
    * Added header control
    
    * Styled header
    
    * Added home view control
    
    * Created state for App and added routes that ppass some of the state to other components
    
    * Added different headers depending on the user
    
    * Added different Home views depending on the user
    
    * Added Login view control
    
    * Polished the header style a bit more
    
    * Fixed small bug with links
    
    * Deleted "Register" link because it is available @ "SIgn Up"
    
    * Redirects to /login upon successful submit
    
    * showCart prints cart to console
    
    * created Register.css
    
    * button customization
    
    * added register button
    
    * added login button
    
    * updated
    
    * put in correct import
    
    * updating Register css
    
    * Schemas are linked. Ready for post/get testing
    
    * Added an order schema to hold paid orders from a user for the vendor to see, i.e. user cart --> order cart --> vendor orders

---
+++ server/models/baseSchemas/OrderSchema.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+var orderSchema = new Schema ({
+    id: {type: String, required: true, unique = true},
+    userID: {type: String, required: true},     // Links to user ID
+    paidCart: [ {type: String} ]                // Array of item IDs
+});
