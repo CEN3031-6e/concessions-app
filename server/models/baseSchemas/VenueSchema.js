83d2c67ac46a32e177ed8a002b02ec46c4d0652a
Charles Draper <charlesdraper@ufl.edu>
2019-11-20 14:45:38 -0500

    Updating venue schema

--- server/models/baseSchemas/VenueSchema.js
+++ server/models/baseSchemas/VenueSchema.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
-const bcrypt = require("bcryptjs");
 
-const venueSchema = new Schema({
-    id: {type: String, required: true},
-    name: {type: String, required: true},
-    address: {type: String, required: true},
-    vendors: [
-       {type: String}   // Array of strings of vendor ID's 
-    ]
-
+const venueSchema = mongoose.Schema({
+        name: {type: String, required: true},
+        address: {type: String, required: true},
+        Date: {type: Date, default: Date.now()},
+        vendors: [{id: {type: String, required: true}, name: {type: String, required: true}}],
 });
+
+module.exports = mongoose.model("venue", venueSchema);
