dee879657619ca378050fade3b158a9d7238fb44
Charles Draper <charlesdraper@ufl.edu>
2019-11-20 14:45:48 -0500

    Updating admin routes

--- server/config/express.js
+++ server/config/express.js
@@ -8,7 +8,8 @@ const path = require('path'),
     morgan = require('morgan'),
     bodyParser = require('body-parser'),
     exampleRouter = require('../routes/examples.server.routes'),
-    userRouter = require('../routes/users.server.routes');
+    userRouter = require('../routes/users.server.routes'),
+    adminRouter = require('../routes/admin.server.routes');
 
 module.exports.init = () => {
     mongooseSetup.start(); //starts the database
@@ -57,6 +58,7 @@ module.exports.init = () => {
     //Routes
     app.use('/api/example', exampleRouter);
     app.use('/users', userRouter);
+    app.use('/admin', adminRouter);
 
     if (process.env.NODE_ENV === 'production') {
         // Serve any static files
