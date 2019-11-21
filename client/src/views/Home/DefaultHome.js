aff8baba05ff03b8a362f2928884ac76c540e67f
Zahin Ibnat <ibnatz16@ufl.edu>
2019-11-21 10:42:51 -0500

    customized home and login

--- client/src/views/Home/DefaultHome.js
+++ client/src/views/Home/DefaultHome.js
@@ -3,7 +3,7 @@ import React from 'react';
 function DefaultHome() {
     return (
         <div className="App">
-        	<h1 class="display-1">Welcome to Vendr</h1>
+        	<h1 class="h1" >Welcome to Vendr</h1>
             <h2 class="defaultSlogan">Tired of the long lines? Don't stress!</h2>
         </div>
     );
