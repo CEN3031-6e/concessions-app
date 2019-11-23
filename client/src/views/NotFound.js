73cc22beaeba0034f10cdae92863491cb9cba601
Charles Draper <charlesdraper@ufl.edu>
2019-11-23 15:19:12 -0500

    Simplified NotFound.js

--- client/src/views/NotFound.js
+++ client/src/views/NotFound.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const NotFound = () => {
-        return <p>"Page not found"</p>
+export default () => {
+        return <p>Page not found</p>
 }
-
-export default NotFound;
