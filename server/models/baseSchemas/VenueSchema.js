9462fa3f007c40570a430f595371bad27c3442ed
Charles Draper <charlesdraper@ufl.edu>
2019-11-23 15:20:03 -0500

    Updated schema

--- server/models/baseSchemas/VenueSchema.js
+++ server/models/baseSchemas/VenueSchema.js
@@ -1,10 +1,34 @@
 const mongoose = require('mongoose');
 
 const venueSchema = mongoose.Schema({
-        name: {type: String, required: true},
-        address: {type: String, required: true},
-        Date: {type: Date, default: Date.now()},
-        vendors: [{id: {type: String, required: true}, name: {type: String, required: true}}],
+    name: {type: String, required: true}, //Name of the venue
+    address: {type: String, required: true}, //Street address of the venue
+    vendors: [ //Array of vendors at the venue
+        {
+            name: {type: String, required: true}, // Name of the vendor
+            venueID: {type: String, required: true}, //ID of the wrapping venue
+            goods: [ //Array of goods for sale
+                {
+                    name: {type: String, required: true}, //Name of good for sale
+                    vendorID: {type: String, required: true}, //ID of the wrapping vendor
+                    price: {type: String, required: true}, //Price of good for sale
+                    quantity: {type: String, required: true} //Quantity of good available
+                }
+            ],
+            orders: [ //Array of orders in the queue
+                {
+                    userID: {type: String, required: true}, //ID of user who placed order
+                    cart: [ //Array of goods in the order
+                        {
+                            name: {type: String, required: true}, //Name of good in order
+                            price: {type: String, required: true}, //Price of good in order
+                            quantity: {type: String, required: true} //Quantity of good in order
+                        }
+                    ]
+                }
+            ]
+        }
+    ],
 });
 
 module.exports = mongoose.model("venue", venueSchema);
