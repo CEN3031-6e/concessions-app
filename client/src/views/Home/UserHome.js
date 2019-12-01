47c21b90e44f899622ce1dd505c64f7e27ecdad8
Zahin Ibnat <ibnatz16@ufl.edu>
2019-12-01 13:51:14 -0500

    changed the styling in user home

--- client/src/views/Home/UserHome.js
+++ client/src/views/Home/UserHome.js
@@ -3,12 +3,12 @@ import React from 'react';
 function UserHome(props) {
     return (
         <div className="App">
-        	<p>Hello, {props.user.name}! </p>
-        	<p> 
+        	<h1 className = "h2">Hello, {props.user.name}! </h1>
+        	<h2 className = "defaultSlogan"> 
         	As a customer, you can select venues, vendors, and goods. 
         	It is also possible to view your order history. 
         	Go ahead and give Vendr a try!
-        	</p>
+        	</h2>
         </div>
     );
 }
