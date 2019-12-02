ea807779fd541bc936b1699200b2383f9cbdb4e2
ywilliamson20 <ywilliamson20@ufl.edu>
2019-12-02 02:53:58 -0500

    Added form for card

--- client/src/components/User/Checkout.js
+++ client/src/components/User/Checkout.js
@@ -4,15 +4,27 @@ import {Button} from 'react-bootstrap'
 import Form from 'react-bootstrap/Form'
 import Card from 'react-bootstrap/Card'
 import ListGroup from 'react-bootstrap/ListGroup'
+import FormControl from 'react-bootstrap/FormControl'
+import { Row, Col, Grid } from 'react-bootstrap';
 
-export default (props) => {
+class Checkout extends React.Component {
+  constructor(props) {
+      super(props);
 
-       let subtotal = 0;
-       for (let good of props.cart) {
-           subtotal += (good.price * good.quantity);
-       }
+      this.state = {
+        card: false,
+      }
+  }
+    addCard=()=>{
+    this.setState({card:!this.state.card})
+  }
 
 
+render() {
+  let subtotal = 0;
+  for (let good of this.props.cart) {
+      subtotal += (good.price * good.quantity);
+  }
     return (
 
       <div>
@@ -22,7 +34,7 @@ export default (props) => {
 
               <Card.Text>
                 <ListGroup>
-                    {props.cart.map((good) =>
+                    {this.props.cart.map((good) =>
 
                         <ListGroup.Item variant="light">{good.name} x{good.quantity} - ${(good.price*good.quantity).toFixed(2)}</ListGroup.Item>)}
                         <ListGroup.Item >SUBTOTAL: ${subtotal.toFixed(2)}</ListGroup.Item>
@@ -33,14 +45,40 @@ export default (props) => {
           </Card>
         <div>
           <Button style={{position: 'absolute',  textAlign:'center',
-          right:800}}> Link your card </Button>
+          right:800}} onClick={this.addCard}> Link your card </Button>
           <Button style={{position: 'absolute',
           right:650}}> Pay for Order </Button>
+        {this.state.card ?
 
+          <Form style={{position: 'absolute',  textAlign:'center',
+          left:400, bottom:0}}>
+          <Card style={{backgroundColor: 'Aquamarine'}}>
+                <Form.Label>ADD CARD</Form.Label>
+                <Form.Control placeholder="Full name" />
+                <Form.Control placeholder="Card Number" />
+                <Row>
+                <Col>
+                  <Form.Label>Valid Thru</Form.Label>
+                  <Form.Control placeholder="00/00" />
+                  </Col>
+                  <Col>
+                  <Form.Label>CVC</Form.Label>
+                  <Form.Control placeholder="CVC" />
+                  </Col>
+                  </Row>
+                  <Button onClick={this.addCard}>
+                      Submit
+                      </Button>
+                        </Card>
+          </Form>
 
+          : ' '}
         </div>
+
+
       </div>
     )
-
+}
 
 }
+export default Checkout;
