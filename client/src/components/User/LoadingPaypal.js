4e2f57eab692f0baca8e9115e92cce5f31ca69f3
Rafael Alfonzo <54005106+solciel@users.noreply.github.com>
2019-12-03 12:54:40 -0500

    last change

--- client/src/components/User/LoadingPaypal.js
+++ client/src/components/User/LoadingPaypal.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import axios from 'axios'
-import { Link } from 'react-router-dom'
+import { Router, Route, Switch, Redirect  } from 'react-router-dom'
 
 class LoadingPaypal extends React.Component {
 
@@ -15,30 +15,41 @@ class LoadingPaypal extends React.Component {
   		let search = window.location.search;
 		let params = new URLSearchParams(search);
 		let payment = params.get('paymentId');
-		let payer = params.get('PayerID');
+		let Payer = params.get('PayerID');
 
 		let request = {
 			paymentId: payment,
-			payerID: payer
+			PayerID: Payer
 		}
 
 
-		console.log("Executing payment.");
-		axios.post('/users/executepayment', request).then(res => {
-			console.log(res);
-			if (res.data.success) this.setState({p1: <p> Payment successful. Order has been processed. </p>} );
-			else this.setState({p1: <p> There was a failure while processing payment. </p>} );	 		
-		});
+		 axios.post('/users/executepayment', request).then(res => {
+
+		 		console.log(res);
+		 		if (res.data.success === true) {
+		 			this.setState({p1: <p> Payment successful. Order has been processed. </p>} ); 
+		 		}
+		 		else {
+		 			this.setState({p1: <p> There was a failure while processing payment. </p>} ); 
+		 		}	 		
+		 });
+
   	}
 
 
-	render() {
+	 render() {
+
+
 	 	return (
-			<div>
-				{this.state.p1}
-			</div>
-	 	);
-	} 
+	 	<div>
+	 		{this.state.p1}
+	 	</div>
+
+	 );
+
+
+	 }
+
 }
 
-export default LoadingPaypal;
+export default LoadingPaypal; 
