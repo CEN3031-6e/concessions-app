17bfbba8832adbb4a6eef463e4dd3e38bc9cad1e
Rafael Alfonzo <54005106+solciel@users.noreply.github.com>
2019-12-03 03:46:30 -0500

    adjusted file to work on heroku

--- server/routes/users.server.routes.js
+++ server/routes/users.server.routes.js
@@ -206,8 +206,8 @@ router.post("/register", (req, res) => {
         "payment_method": "paypal"
     },
     "redirect_urls": {
-        "return_url": "http://localhost:3000/success",
-        "cancel_url": "http://localhost:3000/failure"
+        "return_url": "https://onlinevendr.herokuapp.com/success",
+        "cancel_url": "https://onlinevendr.herokuapp.com/failure"
     },
     "transactions": [{
         "item_list": {
