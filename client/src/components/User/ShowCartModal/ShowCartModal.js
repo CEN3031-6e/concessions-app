5b2c9469e46d9763bcda8dadd50b37c9b2eb65c6
ywilliamson20 <ywilliamson20@ufl.edu>
2019-12-02 21:48:05 -0500

    Delete Good

--- client/src/components/User/ShowCartModal/ShowCartModal.js
+++ client/src/components/User/ShowCartModal/ShowCartModal.js
@@ -2,8 +2,22 @@ import React from 'react'
 import Backdrop from '../../UI/Backdrop/Backdrop'
 import {Button} from 'react-bootstrap'
 import './ShowCartModal.css'
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
 
 class ShowCartModal extends React.Component {
+
+  constructor(props) {
+      super(props);
+
+      this.state = {
+          deleteG:[],
+          please :0,
+          notify: false,
+          cart: this.props.cart
+
+      }
+  }
 
     modalClose = () => this.props.modalClose();
 
@@ -11,7 +25,15 @@ class ShowCartModal extends React.Component {
 
     clearCart = () => this.props.clearCart();
 
+    deletingG = (id) =>{
+      this.state.please= this.props.cart.findIndex(good => good.id === this.state.deleteG[0])
 
+        }
+
+      deleted =()=>{
+        this.props.cart.splice(this.state.please,1)
+        this.setState({notify:true})
+      }
     render() {
 
         let subtotal = 0;
@@ -30,14 +52,21 @@ class ShowCartModal extends React.Component {
                     }}>
                     <center>
                     <h1>This is your cart</h1>
-                    {this.props.cart.map((good) => <div key={good.id}>{good.name} x{good.quantity} - ${good.price.toFixed(2)}</div>)}
-                    <br />
+                    {this.state.notify ?
+                    <p style={{backgroundColor: 'white'}}> Item has been deleted.</p>:null}
+                      <ListGroup>
+                    {this.props.cart.map((good) => <ListGroup.Item onClick={()=>{this.deletingG(good.id)}}  ><div key={good.id}>{good.name} x{good.quantity} - ${good.price.toFixed(2)}</div></ListGroup.Item >)}
+
+
+                    </ListGroup>
                     <p>Subtotal: ${subtotal.toFixed(2)}</p>
                     <Button id={'submit'} onClick={this.submitCart.bind(this)}>Submit Cart</Button>
                     <Button id={'return'} onClick={this.modalClose.bind(this)}>Return</Button>
                     <Button id={'clear'} onClick={this.clearCart.bind(this)}>Clear Cart</Button>
+                    <Button id={'delete'} onClick={this.deleted.bind(this)}>Delete</Button>
                     </center>
                 </div>
+
             </div>
         );
     }
