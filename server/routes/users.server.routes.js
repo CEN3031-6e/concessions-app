7edee5f0eb28c8d417f3f34fbcf613ef8774b43c
Charles Draper <charlesdraper@ufl.edu>
2019-12-03 10:25:58 -0500

    Moved submitOrder to after PayPal execution

--- server/routes/users.server.routes.js
+++ server/routes/users.server.routes.js
@@ -102,106 +102,104 @@ router.post("/register", (req, res) => {
         });
       });
     });
-  });
+});
 
-  //Login handle (done through passport)
-  router.post("/login", passport.authenticate("user-login"), (req, res) => {
-    req.session.userId = req.user._id;
-    res.locals.user = req.user;
-    res.locals.session = req.session;
-    return res.send({
-      success: true,
-      message: "successful login",
-      user: req.user
-    })
+//Login handle (done through passport)
+router.post("/login", passport.authenticate("user-login"), (req, res) => {
+  req.session.userId = req.user._id;
+  res.locals.user = req.user;
+  res.locals.session = req.session;
+  return res.send({
+    success: true,
+    message: "successful login",
+    user: req.user
   })
+})
 
-  //Logout handle
-  router.post("/logout", (req, res) => {
-    req.session.destroy(err => {
-      if (err) return res.send({
-        success: false,
-        message: "Server error: couldn't destroy session (log user out)"
-      });
-      req.logout();
-      res.clearCookie("sid").send({
-        success: true,
-        message: "Successfully logged out"
-      });
+//Logout handle
+router.post("/logout", (req, res) => {
+  req.session.destroy(err => {
+    if (err) return res.send({
+      success: false,
+      message: "Server error: couldn't destroy session (log user out)"
+    });
+    req.logout();
+    res.clearCookie("sid").send({
+      success: true,
+      message: "Successfully logged out"
     });
   });
+});
 
 
-  //Users posting their cart to the database
-  router.post("/addOrder", (req, res) => {
-    const { user, venueID, vendor, cart, subtotal, completed } = req.body;
+//Users posting their cart to the database
+router.post("/addOrder", (req, res) => {
+  const { user, venueID, vendor, cart, subtotal, completed } = req.body;
 
-    const userID = user.id, userName = user.name, userEmail = user.email;
-    const vendorID = vendor.id, vendorName = vendor.name;
+  const userID = user.id, userName = user.name, userEmail = user.email;
+  const vendorID = vendor.id, vendorName = vendor.name;
 
-    const vendorOrder = {
-      userID,
-      userName,
-      userEmail,
-      cart,
-      subtotal,
-      completed
-    };
-    Venue.findOneAndUpdate({'_id': venueID, 'vendors._id': vendorID}, {$push: {'vendors.$.orders': vendorOrder}}, {new: true}, function(err, venue) {
-      if (err) return res.send({
-        success: false,
-        message: "Failed to post order to vendor"
-      });
-      else {
-        const vendor = venue.vendors.find(vendor => vendor._id == vendorID);
-        const linkedID = vendor.orders ? vendor.orders[vendor.orders.length - 1]._id : "";
-        
-        const userOrder = {
-          linkedID,
-          vendorID,
-          vendorName,
-          cart,
-          subtotal,
-          completed
-        };
-        User.findOneAndUpdate({'_id': user.id}, {$push: {'orders': userOrder}}, {new: true}, function(err, newdata) {
-          if (err) return res.send({
-            success: false,
-            message: "Failed to post order to user"
-          });
-          else return res.send({
-            success: true,
-            message: "Double success"
-          });
-        })
-      }
-    })
+  const vendorOrder = {
+    userID,
+    userName,
+    userEmail,
+    cart,
+    subtotal,
+    completed
+  };
+  Venue.findOneAndUpdate({'_id': venueID, 'vendors._id': vendorID}, {$push: {'vendors.$.orders': vendorOrder}}, {new: true}, function(err, venue) {
+    if (err) return res.send({
+      success: false,
+      message: "Failed to post order to vendor"
+    });
+    else {
+      const vendor = venue.vendors.find(vendor => vendor._id == vendorID);
+      const linkedID = vendor.orders ? vendor.orders[vendor.orders.length - 1]._id : "";
+      
+      const userOrder = {
+        linkedID,
+        vendorID,
+        vendorName,
+        cart,
+        subtotal,
+        completed
+      };
+      User.findOneAndUpdate({'_id': user.id}, {$push: {'orders': userOrder}}, {new: true}, function(err, newdata) {
+        if (err) return res.send({
+          success: false,
+          message: "Failed to post order to user"
+        });
+        else return res.send({
+          success: true,
+          message: "Double success"
+        });
+      })
+    }
   })
+})
 
-  //Retrieving user's orders
-  router.get('/orders', (req, res) => {
-    User.findOne({'_id': req.query.userID}, function(err, user) {
-      if (err) return res.send({
-        success: false,
-        orders: []
-      });
-      else return res.send({
-        success: true,
-        orders: user.orders
-      });
-    })
+//Retrieving user's orders
+router.get('/orders', (req, res) => {
+  User.findOne({'_id': req.query.userID}, function(err, user) {
+    if (err) return res.send({
+      success: false,
+      orders: []
+    });
+    else return res.send({
+      success: true,
+      orders: user.orders
+    });
   })
+})
 
-  //register payment
-  router.post('/pay', (req, res) => {
+//Register payment
+router.post('/pay', (req, res) => {
 
+    let {subtotal} = req.body;
+    currentSubtotal = subtotal.toString();
 
-     let {subtotal} = req.body;
-
-     currentSubtotal = subtotal.toString();
-
-     var defPayment = {
-      "intent": "sale",
+    var defPayment = {
+    "intent": "sale",
     "payer": {
         "payment_method": "paypal"
     },
@@ -225,77 +223,65 @@ router.post("/register", (req, res) => {
         },
         "description": "Order at Vendr"
     }]
-};
+  };
 
-    paypal.payment.create(defPayment, function(error, payment){
-
-      if(error){
-        console.error(JSON.stringify(error));
-      } else {
-
-        for(let i = 0; i < payment.links.length; i++){
-          if (payment.links[i].rel === 'approval_url') {
-
-            //res.redirect(payment.links[i].href);
-            res.send({paypal_url: payment.links[i].href});
-          }
+  paypal.payment.create(defPayment, function(error, payment) {
+    if (error) {
+      console.error(JSON.stringify(error));
+    } else {
+      for(let i = 0; i < payment.links.length; i++){
+        if (payment.links[i].rel === 'approval_url') {
+          //res.redirect(payment.links[i].href);
+          res.send({paypal_url: payment.links[i].href});
         }
-        
-      }
+      }  
+    }
   });
-
-  })
-
-
-   router.post('/executepayment', (req, res) => {
-
-console.log()
-console.log()
-console.log()
-    console.log('executepayment')
-
-      console.log(req.body);
-      
-      const paymentId = req.body.paymentId;
-      const payerId = req.body.PayerID;
+})
 
 
-      console.log('paymentId')
-console.log(paymentId)
-console.log('payerId')
-console.log(payerId)
+router.post('/executepayment', (req, res) => {
 
-    const execute_payment = {
+  console.log()
+  console.log()
+  console.log()
+  console.log('executepayment')
+  console.log(req.body);
+    
+  const paymentId = req.body.paymentId;
+  const payerId = req.body.payerID;
+
+  console.log('paymentId')
+  console.log(paymentId)
+  console.log('payerId')
+  console.log(payerId)
+
+  const execute_payment = {
     payer_id: payerId,
     transactions: [{
         amount: {
-            currency: "USD",
-            total: currentSubtotal
+          currency: "USD",
+          total: currentSubtotal
         }
     }]
   };
 
-    console.log('subtotal')
-    console.log(currentSubtotal);
+  console.log('subtotal')
+  console.log(currentSubtotal);
 
-      
-
-      paypal.payment.execute(paymentId, execute_payment, function(error, payment){
-          if(error){
-            console.error(JSON.stringify(error));
-          } else {
-            if (payment.state == 'approved'){
-              console.log('payment completed successfully');
-              res.send({success: true});
-            } else {
-              console.log('payment not successful');
-              res.send({success: false});
-            }
-          }
-      });
-
-   })
-
-
+  paypal.payment.execute(paymentId, execute_payment, function(error, payment) {
+    if (error) {
+      console.error(JSON.stringify(error));
+    } else {
+      if (payment.state == 'approved') {
+        console.log('payment completed successfully');
+        res.send({success: true});
+      } else {
+        console.log('payment not successful');
+        res.send({success: false});
+      }
+    }
+  });
+})
 
 module.exports = router;
