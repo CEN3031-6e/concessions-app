199ea7ba890c07fe93265cd03bbb2c800cb2e646
ProfSanjay <32143329+ProfSanjay@users.noreply.github.com>
2019-12-07 16:52:46 -0500

    Cleanup (#63)
    * Cleanup (#55) (#56)
    
    * Added delete venue/vendor functionality
    
    * Touched up buttons and inputs
    
    * Delete background-img.jpg
    
    * Delete background2.png
    
    * Delete background.png
    
    * got rid of unused code
    
    * deleted vendr logo not needed in this folder
    
    * added font style for registration title
    
    * updated the font style for the title of page
    
    * changed font size because it was too small
    
    * updated font size
    
    * Delete Good
    
    * Added delete to checkout, pay order button takes back to beginning
    
    * Forgot Password Message popup
    
    * Added all PayPal functionality.
    
    * fixed conflict
    
    * updated dependencies (paypal sdk)
    
    * updated depencencies again
    
    * adjusted file to work on heroku
    
    * Styled pay button
    
    * styled button to be more symmetrical
    
    * styling
    
    * styling
    
    * Moved submitOrder to after PayPal execution
    
    * Fixed image, added /failure page
    
    * Removed Link to home
    
    * Update Checkout.js
    
    * Added Bypass button
    
    * Fixed button overlap
    
    * Removing image from register page
    
    * Stopped submitting order from LoadingPaypal
    
    * Submitting order in User.js
    
    * Added check to ensure venue exists
    
    * last change
    
    * Pulled old code
    
    * Changed to heroku links
    
    * Removed bypass
    
    * Cleanups and comments
    
    * Comments on models

--- server/models/VenueSchema.js
+++ server/models/VenueSchema.js
@@ -1,34 +1,40 @@
 const mongoose = require('mongoose');
 
 const venueSchema = mongoose.Schema({
-    name: {type: String, required: true}, //Name of the venue
-    address: {type: String, required: true}, //Street address of the venue
-    vendors: [ //Array of vendors at the venue
+    name: {type: String, required: true},           //Name of the venue
+    address: {type: String, required: true},        //Street address of the venue
+
+    // Array of vendors at the venue
+
+    vendors: [ 
         {
-            name: {type: String, required: true}, // Name of the vendor
-            venueID: {type: String, required: true}, //ID of the wrapping venue
-            goods: [ //Array of goods for sale
+            name: {type: String, required: true},               // Name of the vendor
+            venueID: {type: String, required: true},            //ID of the wrapping venue
+            goods: [                                            //Array of goods for sale
                 {
-                    name: {type: String, required: true}, //Name of good for sale
-                    vendorID: {type: String, required: true}, //ID of the wrapping vendor
-                    price: {type: Number, required: true}, //Price of good for sale
-                    quantity: {type: Number, required: true} //Quantity of good available
+                    name: {type: String, required: true},       //Name of good for sale
+                    vendorID: {type: String, required: true},   //ID of the wrapping vendor
+                    price: {type: Number, required: true},      //Price of good for sale
+                    quantity: {type: Number, required: true}    //Quantity of good available
                 }
             ],
             orders: [
                 {
-                    userID: {type: String, required: true},
-                    userName: {type: String, required: true},
-                    userEmail: {type: String, required: true},
+                    userID: {type: String, required: true},         // Unique ID for a user's order
+                    userName: {type: String, required: true},       // User account username
+                    userEmail: {type: String, required: true},      // User account email
+
+                    // A user's cart that contains the items they want to purchase 
+
                     cart: [
                         {
-                            name: {type: String, required: true},
-                            price: {type: Number, required: true},
-                            quantity: {type: Number, required: true}
+                            name: {type: String, required: true},       // Item Name   
+                            price: {type: Number, required: true},      // Item Price
+                            quantity: {type: Number, required: true}    // Item Quantity
                         }
                     ],
-                    subtotal: {type: Number, required: true},
-                    completed: {type: Boolean, required: true}
+                    subtotal: {type: Number, required: true},           // Subtotal price of all items to be purchased
+                    completed: {type: Boolean, required: true}          // False until order is completed by vendor
                 }
             ]
         }
